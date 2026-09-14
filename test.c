9f378ee87c72698d7cbc7fb9639a0d0b8f62c649
woojin <woojinnj@naver.com>
2026-09-14 11:22:56 +0900

    Q6_A_LL.c solved

--- test.c
+++ test.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
-int main() {
-    printf("Heㅇㄴㅁㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㄴㅁㅇㄴㅁㅇㄴㅁㅇllo, DevContainer!\n");
-    return 0;
+#include <stdlib.h>
+#include <stddef.h>
+
+//////////////////////////////////////////////////////////////////////////////////
+
+typedef struct _listnode
+{
+    int item;
+    struct _listnode *next;
+} ListNode;
+
+typedef struct _linkedlist
+{
+    int size;
+    ListNode *head;
+} LinkedList;
+
+int main()
+{
+    ListNode node = {0, NULL};
+    LinkedList ll = {1, &node};
+
+    LinkedList *ll_pointer = (LinkedList *)((char *)&ll.head - offsetof(LinkedList, head));
+    
+    printf("&ll        = %p\n", (void *)&ll);
+    printf("&ll.head   = %p\n", (void *)&ll.head);
+    printf("ll.head    = %p\n", (void *)ll.head);
+    printf("&node      = %p\n", (void *)&node);
+    printf("ll_pointer = %p\n", (void *)ll_pointer);
+
 }
